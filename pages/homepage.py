c5a592be1d20849ca708a8b31561f35eb88d1591
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2025-07-08 19:41:48 +0200

    added homepage.py file in pages folder

--- pages/homepage.py
+++ pages/homepage.py
@@ -1,1 +1,9 @@
- 
+from pages.base_page import BasePage
+
+class HomePage(BasePage):
+    def __init__(self, page):
+        super().__init__(page)
+
+
+    def go_to_homepage(self):
+        self.visit("https://demo.prestashop.com/#/en/front") 
