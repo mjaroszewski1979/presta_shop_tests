a43162abe30c40dd96f101d05d6912649e0b9272
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2025-07-22 20:02:02 +0200

    added comments in home_page_utils.py file

--- utils/home_page_utils.py
+++ utils/home_page_utils.py
@@ -1,7 +1,18 @@
 from faker import Faker
 
+# Initialize Faker instance for generating fake data
 fake = Faker()
 
 def generate_unique_email(domain="gmail.com") -> str:
+    """
+    Generates a unique email address using Faker's username generator.
+
+    Args:
+        domain (str): The email domain to append. Defaults to "gmail.com".
+
+    Returns:
+        str: A unique email address in the format '<username>@<domain>'.
+    """
+    
     username = fake.user_name()
     return f"{username}@{domain}"
