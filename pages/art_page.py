d5d346cfba1df82f66ecbea01490f4e936a2e64c
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2025-07-22 19:52:22 +0200

    added comments in art_page.py file

--- pages/art_page.py
+++ pages/art_page.py
@@ -2,6 +2,14 @@ from pages.base_page import BasePage
 from locators.art_page_locators import ArtPageLocators
 
 class ArtPage(BasePage):
+    """
+    Page Object Model for the Art category page.
+
+    Provides access to UI elements and interactions specific to the Art category.
+    The page content is rendered within an iframe identified by '#framelive'.
+    Inherits common functionality from BasePage.
+    """
+    
     def __init__(self, page):
         super().__init__(page)
         self.frame = page.frame_locator("#framelive")
