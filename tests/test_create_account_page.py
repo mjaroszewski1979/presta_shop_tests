f22c2bc804cb0236870372d2cf1a1a698f9558a4
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2025-07-26 08:48:52 +0200

    added new method -test_social_title_label_is_visible_with_text in test_create_account_page.py file

--- tests/test_create_account_page.py
+++ tests/test_create_account_page.py
@@ -12,3 +12,14 @@ def test_social_title_label_is_visible_with_text(home_page, sign_page, create_ac
     sign_page.create_account_link.click()
     create_account_page.form_labels.first.wait_for(state="visible", timeout=5000)
     expect(create_account_page.form_labels.first).to_contain_text('Social title')
+
+def test_first_name_label_is_visible_with_text(home_page, sign_page, create_account_page):
+    """
+    Verify the visibility and label of the 'Explore back office' button.
+    """
+    home_page.sign_in_span.wait_for(state="visible", timeout=5000)
+    home_page.sign_in_span.click()
+    sign_page.create_account_link.wait_for(state="visible", timeout=5000)
+    sign_page.create_account_link.click()
+    create_account_page.form_labels.nth(1).wait_for(state="visible", timeout=5000)
+    expect(create_account_page.form_labels.nth(1)).to_contain_text('First name')
