3937c381fe7cca377bb38358a2f040d9c5b638d1
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2025-07-27 21:00:30 +0200

    added new method -test_last_name_label_is_visible_with_text in test_create_account_page.py file

--- tests/test_create_account_page.py
+++ tests/test_create_account_page.py
@@ -23,3 +23,14 @@ def test_first_name_label_is_visible_with_text(home_page, sign_page, create_acco
     sign_page.create_account_link.click()
     create_account_page.form_labels.nth(1).wait_for(state="visible", timeout=5000)
     expect(create_account_page.form_labels.nth(1)).to_contain_text('First name')
+
+def test_last_name_label_is_visible_with_text(home_page, sign_page, create_account_page):
+    """
+    Verify the visibility and label of the 'Explore back office' button.
+    """
+    home_page.sign_in_span.wait_for(state="visible", timeout=5000)
+    home_page.sign_in_span.click()
+    sign_page.create_account_link.wait_for(state="visible", timeout=5000)
+    sign_page.create_account_link.click()
+    create_account_page.form_labels.nth(2).wait_for(state="visible", timeout=5000)
+    expect(create_account_page.form_labels.nth(2)).to_contain_text('Last name')
