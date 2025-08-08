7d1a34dbb561c75d4abbff7fdd7183c17bb392f0
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2025-08-08 21:51:17 +0200

    added comments in test_cart_page.py file

--- tests/test_cart_page.py
+++ tests/test_cart_page.py
@@ -10,7 +10,20 @@ from utils.assertions import assert_element_visible_with_text
 
 def test_removing_product_from_cart_is_working(home_page, first_product_page, cart_page):
     """
-    Validates that the 'Create Account' form is visible and all expected labels are correctly displayed.
+    UI Test: Verify that removing a product from the cart works as expected.
+
+    Steps:
+        1. Wait for the first product link on the home page to become visible and click it.
+        2. Wait for the "Add to cart" button on the product page to be visible and click it.
+        3. Wait for the "Continue shopping" button to appear and click it to return to the home page.
+        4. Click on the cart icon to view the cart contents.
+        5. Wait for the "Remove from cart" link to be visible and click it.
+        6. Verify that the cart is empty by asserting the visibility of the confirmation message.
+
+    Args:
+        home_page: Page Object for the home page.
+        first_product_page: Page Object for the first product details page.
+        cart_page: Page Object for the cart page.
     """
     home_page.first_product_link.first.wait_for(state="visible", timeout=10000)
     home_page.first_product_link.first.click()
