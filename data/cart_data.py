e81f2a769ef6dbfa59f2562ffb4b0b66d5ab7c02
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2025-08-11 20:57:35 +0200

    added new method -test_personal_info_form_is_visible_with_text in test_cart_page.py file

--- data/cart_data.py
+++ data/cart_data.py
@@ -16,3 +16,29 @@ def get_sign_in_form_data(cart_page):
         (cart_page.forgot_password_link, "Forgot your password?"),
 
     ]
+def get_personal_info_form_data(cart_page):
+    """
+    Returns a list of tuples mapping each label element in the 'Contact' form
+    to its expected display text. This is used to validate that form labels are 
+    visible and contain correct content.
+
+    Args:
+        contact_page: An instance of the ContactPage with form label locators.
+
+    Returns:
+        list[tuple]: A list of (locator, expected_text) pairs for label verification.
+    """
+    return [
+        (cart_page.social_title_label, "Social title"),
+        (cart_page.first_name_label, "First name"),
+        (cart_page.last_name_label, "Last name"),
+        (cart_page.email_label, "Email"),
+        (cart_page.password_label, "Password"),
+        (cart_page.birthday_label, "Birthdate"),
+        (cart_page.form_info_para, "Create an account"),
+        (cart_page.checkbox_labels.first, "Receive offers from our partners"),
+        (cart_page.checkbox_labels.nth(1), "I agree to the terms and conditions and the privacy policy"),
+        (cart_page.checkbox_labels.nth(2), "Sign up for our newsletter"),
+        (cart_page.checkbox_labels.nth(3), "Customer data privacy"),
+        (cart_page.continue_button, "Continue"),
+    ]
--- tests/test_cart_page.py
+++ tests/test_cart_page.py
@@ -4,7 +4,7 @@ from playwright.sync_api import expect
 # Import custom utility function to assert visibility and text of multiple elements.
 from utils.assertions import assert_element_visible_with_text, assert_elements_visible_with_texts
 
-from data.cart_data import get_sign_in_form_data
+from data.cart_data import get_sign_in_form_data, get_personal_info_form_data
 
 
 
@@ -90,6 +90,31 @@ def test_checkout_with_sign_in_option_is_working(home_page, first_product_page, 
     cart_page.sign_in_link.wait_for(state="visible", timeout=10000)
     cart_page.sign_in_link.click()
     locators_and_texts = get_sign_in_form_data(cart_page)
+    assert_elements_visible_with_texts(locators_and_texts)
+
+def test_personal_info_form_is_visible_with_text(home_page, first_product_page, cart_page):
+    """
+    UI Test: Verify that the product details section is accessible and displays the expected composition information.
+
+    Steps:
+        1. Wait for the first product link on the home page to be visible and click it.
+        2. Wait for the 'Product details' link to be visible and click it.
+        3. Retrieve expected locators and text values for the composition section.
+        4. Assert that all retrieved elements are visible and contain the expected text.
+
+    Args:
+        home_page: Page Object for the home page.
+        first_product_page: Page Object for the first product's details page.
+    """
+    home_page.first_product_link.first.wait_for(state="visible", timeout=10000)
+    home_page.first_product_link.first.click()
+    first_product_page.add_to_cart_button.wait_for(state="visible", timeout=10000)
+    first_product_page.add_to_cart_button.click()
+    first_product_page.checkout_link.wait_for(state="visible", timeout=10000)
+    first_product_page.checkout_link.click()
+    cart_page.proceed_to_checkout_link.wait_for(state="visible", timeout=10000)
+    cart_page.proceed_to_checkout_link.click()
+    locators_and_texts = get_personal_info_form_data(cart_page)
     assert_elements_visible_with_texts(locators_and_texts)
 
 
