49646b6bad7d3309ff6aa8d4d09f007f36c6512a
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2025-09-02 19:55:12 +0200

    added code comments in clothes_page_locators.py file

--- locators/clothes_page_locators.py
+++ locators/clothes_page_locators.py
@@ -1,6 +1,11 @@
 class ClothesPageLocators:
     """
-    CSS selectors for locating UI elements on the Clothes category page.
-    Used in the ClothesPage Page Object.
+    Collection of CSS selectors for the Clothes category page.
+
+    These locators are used to identify and interact with UI elements 
+    specific to the Clothes category view, typically referenced in the 
+    ClothesPage Page Object.
     """
+
+    # Header element for the Clothes category product list
     CLOTHES_DIV_HEADER = 'div#js-product-list-header h1'
