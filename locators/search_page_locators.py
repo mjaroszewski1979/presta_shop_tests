4740244093fda545cb67b25770c627f82f662f09
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2025-09-02 20:12:25 +0200

    added code comments in sign_page_locators.py file

--- locators/search_page_locators.py
+++ locators/search_page_locators.py
@@ -3,4 +3,8 @@ class SearchPageLocators:
     CSS selectors for locating UI elements on the search results page.
     Used in the SearchPage Page Object.
     """
+
+    # -------------------------------
+    # Search results information
+    # -------------------------------
     TOTAL_PRODUCTS_PARA = 'div.total-products p'
--- locators/sign_page_locators.py
+++ locators/sign_page_locators.py
@@ -3,10 +3,22 @@ class SignPageLocators:
     CSS selectors for locating UI elements on the sign-in (login) page.
     Used in the SignPage Page Object.
     """
+
+    # -------------------------------
+    # Sign-in form elements
+    # -------------------------------
     SIGN_IN_BUTTON = 'button#submit-login'
     EMAIL_INPUT = 'input#email'
     AUTH_ALERT_LI = 'div.help-block li.alert-danger'
     FORGOT_PASSWORD_LINK = 'div.forgot-password a'
+
+    # -------------------------------
+    # Password reset elements
+    # -------------------------------
     SEND_RESET_LINK_BUTTON = 'button#send-reset-link'
     RESET_PASSWORD_SUCCESS_PARA = 'div#content p'
+
+    # -------------------------------
+    # Account creation navigation
+    # -------------------------------
     CREATE_ACCOUNT_LINK = 'div.no-account a'
