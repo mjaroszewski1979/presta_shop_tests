5c071809ee451f607fdd52b0aa6817339dc41f04
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2025-09-02 19:53:26 +0200

    added code comments in art_page_locators.py file

--- locators/art_page_locators.py
+++ locators/art_page_locators.py
@@ -1,6 +1,11 @@
 class ArtPageLocators:
     """
-    CSS selectors for locating UI elements on the Art category page.
-    Used in the ArtPage Page Object.
+    Collection of CSS selectors for the Art category page.
+
+    These locators are used to identify and interact with UI elements 
+    specific to the Art category view, typically referenced in the 
+    ArtPage Page Object.
     """
+   
+    # Header element for the Art category product list
     ART_DIV_HEADER = 'div#js-product-list-header h1'
