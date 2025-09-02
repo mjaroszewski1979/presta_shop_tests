28e63f733a01b80c940b5e52871509fd9017327b
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2025-09-02 19:50:32 +0200

    added code comments in accessories_page_locators.py file

--- locators/accessories_page_locators.py
+++ locators/accessories_page_locators.py
@@ -1,6 +1,11 @@
 class AccessoriesPageLocators:
     """
-    CSS selectors for locating UI elements on the Accessories category page.
-    Used in the AccessoriesPage Page Object.
-    """
+    Collection of CSS selectors for the Accessories category page.
+
+    These locators are used to identify and interact with UI elements 
+    specific to the Accessories category view, typically referenced in the 
+    AccessoriesPage Page Object.
+    """ 
+
+    # Header element for the Accessories category product list
     ACCESSORIES_DIV_HEADER = 'div#js-product-list-header h1'
